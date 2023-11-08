5dee1619c3d31e8f02e6328b4ccd06788c820901
randypichardo <randypichardo53@gmail.com>
2023-11-08 17:19:43 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(namesarray) {
+const messages = []
+    let i = 0
+    while (i < namesarray.length) {
+    console.log(`Thank you, ${namesarray[i]}, for the wonderful surprise gift!`)
+    messages.push(`Thank you, ${namesarray[i]}, for the wonderful surprise gift!`)
+    i++
+    }
+return messages
+}
+
+function countDown(n) {
+    let i = 0
+    while (i < 1+n) {
+        
+        console.log(n--)
+    
+    }
+}
